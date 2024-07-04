52243d260ca2de7959da475b42d930603fbeba56
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-07-04 14:47:51 +0530

    Unit test generated by RoostGPT
    Using AI Model roostgpt-4-32k

---
+++ core/src/test/java/training/taylor/timetracker/core/Categories.java
@@ -0,0 +1,25 @@
+package training.taylor.timetracker.core;
+
+public interface Categories {
+
+	interface add {
+
+	}
+
+	interface remove {
+
+	}
+
+	interface size {
+
+	}
+
+	interface get {
+
+	}
+
+	interface timeEntries {
+
+	}
+
+}
